5909a77f0d82a3264e68093fa2e054ef4efca529
Jeremias Pak <jeremiaspak@gmail.com>
2020-01-29 23:47:53 -0300

    feat: add rc-switch library

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Weather Sensors
 
 Sends data from weather sensors via 433MHz transmitter.
+
+## Libraries used
+- [rc-switch](https://github.com/sui77/rc-switch/wiki/HowTo_Send)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
 #include <Arduino.h>
+#include <RCSwitch.h>
+
+void publish(String data);
+
+// --------------------- RC Switch ---------------------
+
+#define RF_TRANSMITTER_PIN 10
+RCSwitch rf = RCSwitch();
+void setupRF();
+
+// ----------------------- Setup ------------------------
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
+  setupRF();
 }
 
+// ----------------------- Loop ------------------------
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  publish("1234");
+  delay(1000);
+}
+
+// --------------------- PubSub ------------------------
+
+void publish(String data) {
+  rf.send(data.toInt(), 32);
+  Serial.println(data.toInt());
+}
+
+// -----------------------------------------------------
+//                      RC Switch
+// -----------------------------------------------------
+
+void setupRF() {
+  rf.enableTransmit(RF_TRANSMITTER_PIN);
+  rf.setProtocol(1);
+  rf.setPulseLength(350);
 }
